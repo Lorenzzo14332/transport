d9df8b4973bc4243bbf5cca9441d2c61b3fe172a
Lorenzzo14332 <lorenzzoalecks@gmail.com>
2022-06-08 19:48:45 -0500

    LOG INN

--- resources/views/frontend/layouts/sidebar.blade.php
+++ resources/views/frontend/layouts/sidebar.blade.php
@@ -47,7 +47,7 @@
       
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item_l">
       <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
         <i class="bi bi-journal-text"></i><span>Extracción Minera</span><i class="bi bi-chevron-down ms-auto"></i>
       </a>
@@ -98,7 +98,31 @@
             <i class="bi bi-circle"></i><span>Ingresos</span>
           </a>
         </li>
+      </ul>
+    </li>
 
+    <li class="nav-item">
+      <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-journal-text"></i><span>Planta Extracción Materia Prima</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="{{ route('graficos.index') }}">
+            <i class="bi bi-circle"></i><span>Graficos</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="{{ route('primas.index') }}">
+            <i class="bi bi-circle"></i><span>Ubicaciones</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="{{ route('primas_ingresos.index') }}">
+            <i class="bi bi-circle"></i><span>Ingresos</span>
+          </a>
+        </li>
       </ul>
     </li>
 
